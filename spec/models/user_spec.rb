04b6e9f4df60047e4109e02cbf9e6f48f13247a8
Jeff Nied <jmnied@gmail.com>
2014-01-26 10:51:27 -0500

    rspec addition

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -62,7 +62,7 @@ describe User do
   end
 
   it { should respond_to(:authenticate) }
-  
+
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
@@ -95,6 +95,6 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
-
   end
+  
 end
