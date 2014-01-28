f7df69aab9956b5cd8167de282a5a156bb886066
Jeff Nied <jmnied@gmail.com>
2014-01-27 22:57:37 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the WP App!"
+      sign_in @user
+    	flash[:success] = "Welcome to the WagesPayable App!"
     	redirect_to @user
     else
       render 'new'
